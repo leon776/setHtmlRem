552449bb786d881534d1cbcea9708d53c8ad00f4
leon <leon776@users.noreply.github.com>
2018-11-02 10:29:39 +0800

    Update setHtmlFontSize.js

--- setHtmlFontSize.js
+++ setHtmlFontSize.js
@@ -9,7 +9,7 @@ console.time("test");
       callback: 字体计算之后的回调函数，可选
     }
     # return Boolean;
-    # xiaoweili@tencent.com
+    # archlichxw@gmail.com
     # ps:请尽量第一时间运行此js计算字体
 */
 !function(win, option) {
